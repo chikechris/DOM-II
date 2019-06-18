719b78ef29f9ab80c4eef2667ded9dc25e855988
Chike Chris <cchike2017@gmail.com>
2019-06-18 15:38:27 -0400

    workd on mouseover in the index.js file

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+//  the FUN BUS logo heading changes color with mouse over
+
+let logoChange = document.querySelector(".logo-heading");
+console.log(logoChange);
+logoChange.addEventListener(
+  "mouseover",
+  function(event) {
+    event.target.style.color = "red";
+    setTimeout(function() {
+      event.target.style.color = "blue";
+    }, 500);
+  },
+  false
+);
