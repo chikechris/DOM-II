dcc8b115dd9cd5eb1020d9d4f53ebfdc8755e37f
Chike Chris <cchike2017@gmail.com>
2019-06-19 02:28:39 -0400

    modified select  and focus events in the index.js file

--- js/index.js
+++ js/index.js
@@ -48,7 +48,74 @@ wheelImg[1].addEventListener(
 // [ ] `drag / drop`
 
 //Load Event
+
 const myImage = document.querySelector(".img-content img");
 myImage.addEventListener("load", function() {
   alert("image  picture has loaded");
 });
+
+// //Resize
+// function displayWindowSize() {
+//   let w = document.documentElement.clientWidth;
+//   let h = document.documentElement.clientHeight;
+
+//   document.getElementById("result").innerHTML =
+//     "Width: " + w + ", " + "Height: " + h;
+// }
+
+// window.addEventListener("resize", displayWindowSize);
+
+// displayWindowSize();
+
+//Scroll Event
+//Alerts you when reach bottom of page
+window.addEventListener("scroll", () => {
+  const sHeight = document.documentElement.scrollHeight - window.innerHeight;
+  const scrolled = window.scrollY;
+
+  if (Math.ceil(scrolled) === sHeight) {
+    alert("You have reached the bottom of the page");
+  }
+});
+
+//DB Click  enlarges h2 when you double click
+
+const dbClick = document.querySelector(".intro h2");
+
+let size = 10;
+let FontSize = size + "px";
+dbClick.addEventListener(
+  "dblclick",
+  function() {
+    size = size * 4;
+    dbClick.style["font-size"] = "120px";
+  },
+  false
+);
+
+const textArea = document.createElement("input");
+textArea.type = "text";
+textArea.value = " Select Your Destination!";
+textArea.style.fontSize = "15px";
+let selectedDestination = document.querySelector(".content-destination h2");
+selectedDestination.appendChild(textArea);
+
+let destinImg = document.querySelector(".content-destination img");
+textArea.addEventListener("select", function() {
+  destinImg.src = "http://source.unsplash.com/random/600x200";
+});
+
+textArea.addEventListener(
+  "focus",
+  function(e) {
+    e.target.style.background = "red";
+  },
+  true
+);
+textArea.addEventListener(
+  "blur",
+  function(e) {
+    e.target.style.background = "grey";
+  },
+  true
+);
